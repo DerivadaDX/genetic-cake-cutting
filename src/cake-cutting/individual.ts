f1b9286aed0fad7bdfedbe80097dda55d8990579
DerivadaDX <derivadadx@gmail.com>
2025-02-11 21:10:56 -0300

    refactor: simplify validation logic in validateChromosome method

--- src/cake-cutting/individual.ts
+++ src/cake-cutting/individual.ts
@@ -9,23 +9,18 @@ export class Individual {
   }
 
   private validateChromosome(chromosome: number[], numberOfAtoms: number): void {
-    if (!Array.isArray(chromosome)) {
+    if (!Array.isArray(chromosome))
       throw new Error('Chromosome must be an array');
-    }
 
-    if (chromosome.some(cut => !Number.isInteger(cut))) {
+    if (chromosome.some(cut => !Number.isInteger(cut)))
       throw new Error('Chromosome must contain only integer values');
-    }
 
-    if (chromosome.some(cut => cut < 0 || cut > numberOfAtoms)) {
+    if (chromosome.some(cut => cut < 0 || cut > numberOfAtoms))
       throw new Error(`Chromosome values must be between 0 and ${numberOfAtoms}`);
-    }
 
-    // Verify cuts are in ascending order
     for (let i = 1; i < chromosome.length; i++) {
-      if (chromosome[i] < chromosome[i - 1]) {
+      if (chromosome[i] < chromosome[i - 1])
         throw new Error('Chromosome cuts must be in ascending order');
-      }
     }
   }
 
