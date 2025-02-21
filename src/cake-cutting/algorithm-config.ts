45e6bd22eeea6fa2a4a3b62a13413d6775d94b93
DerivadaDX <derivadadx@gmail.com>
2025-02-21 09:08:45 -0300

    se crearon tests para AlgorithmConfig y se agregaro validaciones

--- src/cake-cutting/algorithm-config.ts
+++ src/cake-cutting/algorithm-config.ts
@@ -3,6 +3,14 @@ export class AlgorithmConfig {
   public readonly mutationRate: number;
 
   constructor(populationSize: number, mutationRate: number) {
+    if (populationSize <= 0 || !Number.isInteger(populationSize)) {
+      throw new Error('Invalid population size: ' + populationSize);
+    }
+
+    if (mutationRate < 0 || mutationRate > 1) {
+      throw new Error('Invalid mutation rate: ' + mutationRate);
+    }
+
     this.populationSize = populationSize;
     this.mutationRate = mutationRate;
   }
