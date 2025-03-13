bc6f55df2c6ae3e79fd9fa28a15963a91f92ca66
DerivadaDX <derivadadx@gmail.com>
2025-03-13 00:10:57 -0300

    formateo x2

--- tests/cake-cutting/hungarian-algorithm-solver.test.ts
+++ tests/cake-cutting/hungarian-algorithm-solver.test.ts
@@ -7,45 +7,47 @@ describe('HungarianAlgorithmSolver', () => {
     solver = new HungarianAlgorithmSolver();
   });
 
-  test('should solve 2x2 maximization problem', () => {
-    const matrix = [
-      [0.7, 0.3],
-      [0.4, 0.6],
-    ];
-    const result = solver.solveMaximizationProblem(matrix);
-    expect(result).toEqual([
-      { player: 0, portion: 0 },
-      { player: 1, portion: 1 },
-    ]);
-  });
+  describe('solveMaximizationProblem', () => {
+    test('should solve 2x2 maximization problem', () => {
+      const matrix = [
+        [0.7, 0.3],
+        [0.4, 0.6],
+      ];
+      const result = solver.solveMaximizationProblem(matrix);
+      expect(result).toEqual([
+        { player: 0, portion: 0 },
+        { player: 1, portion: 1 },
+      ]);
+    });
 
-  test('should solve 3x3 maximization problem', () => {
-    const matrix = [
-      [0.3, 0.2, 0.5],
-      [0.5, 0.3, 0.2],
-      [0.2, 0.5, 0.3],
-    ];
-    const result = solver.solveMaximizationProblem(matrix);
-    expect(result).toEqual([
-      { player: 0, portion: 2 },
-      { player: 1, portion: 0 },
-      { player: 2, portion: 1 },
-    ]);
-  });
+    test('should solve 3x3 maximization problem', () => {
+      const matrix = [
+        [0.3, 0.2, 0.5],
+        [0.5, 0.3, 0.2],
+        [0.2, 0.5, 0.3],
+      ];
+      const result = solver.solveMaximizationProblem(matrix);
+      expect(result).toEqual([
+        { player: 0, portion: 2 },
+        { player: 1, portion: 0 },
+        { player: 2, portion: 1 },
+      ]);
+    });
 
-  test('should solve 4x4 maximization problem', () => {
-    const matrix = [
-      [0.4, 0.2, 0.1, 0.3],
-      [0.1, 0.3, 0.3, 0.3],
-      [0.3, 0.3, 0.2, 0.2],
-      [0.2, 0.2, 0.4, 0.2],
-    ];
-    const result = solver.solveMaximizationProblem(matrix);
-    expect(result).toEqual([
-      { player: 0, portion: 0 },
-      { player: 1, portion: 3 },
-      { player: 2, portion: 1 },
-      { player: 3, portion: 2 },
-    ]);
+    test('should solve 4x4 maximization problem', () => {
+      const matrix = [
+        [0.4, 0.2, 0.1, 0.3],
+        [0.1, 0.3, 0.3, 0.3],
+        [0.3, 0.3, 0.2, 0.2],
+        [0.2, 0.2, 0.4, 0.2],
+      ];
+      const result = solver.solveMaximizationProblem(matrix);
+      expect(result).toEqual([
+        { player: 0, portion: 0 },
+        { player: 1, portion: 3 },
+        { player: 2, portion: 1 },
+        { player: 3, portion: 2 },
+      ]);
+    });
   });
 });
